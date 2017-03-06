cfacfc6c9d6933d389a19a8f218908a503df0a12
Cristian Ventura <cristian.david.ventura@gmail.com>
2017-03-06 15:00:43 -0500

    first commit

---
+++ video.js
@@ -0,0 +1,20 @@
+<script language="JavaScript" type="text/javascript" src="http://admin.brightcove.com/js/BrightcoveExperiences.js"></script>
+
+<object id="myExperience" class="BrightcoveExperience">
+  <param name="bgcolor" value="#FFFFFF" />
+  <param name="width" value="480" />
+  <param name="height" value="270" />
+  <param name="playerID" value="1257560229001" />
+  <param name="playerKey" value="AQ~~,AAABJJ-pClE~,Ia9m7-1UX4OmIK_AtpI3QceQ0LKttIja" />
+  <param name="isVid" value="true" />
+  <param name="isUI" value="true" />
+  <param name="dynamicStreaming" value="true" />
+  
+</object>
+
+<!-- 
+This script tag will cause the Brightcove Players defined above it to be created as soon
+as the line is read by the browser. If you wish to have the player instantiated only after
+the rest of the HTML is processed and the page load is complete, remove the line.
+-->
+<script type="text/javascript">brightcove.createExperiences();</script>
